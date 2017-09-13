ede9736a40b93028862dc6e993de391f7b15019d
Mariam Nadiradze <nadiradz@grinnell.edu>
2017-09-13 04:10:11 +0000

    see if this works

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,8 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @sort = params[:sort]
+    @movies = Movie.find(:all, order: @sort)
   end
 
   def new
@@ -41,5 +42,6 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
 
 end
